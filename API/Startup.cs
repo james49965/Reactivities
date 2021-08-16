4f1159e9989dcd56622b1429e9483c305e1519d2
james49965 <66869703+james49965@users.noreply.github.com>
2021-08-16 22:40:44 +0100

    Initialised client app project

--- API/Startup.cs
+++ API/Startup.cs
@@ -40,6 +40,12 @@ namespace API
             {
                 options.UseSqlite(_config.GetConnectionString("Default"));
             });
+
+            services.AddCors(c => {
+                c.AddPolicy("CorsPolicy", policy => {
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -55,6 +61,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
